1a4ddd72f206e483c19fc076c5156d23e59ebfd9
caloca <alex.caloca22@gmail.com>
2017-10-19 13:22:13 -0500

    Package

--- halo.go
+++ halo.go
@@ -1,11 +1,16 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "github.com/caloca/stringutil"
+)
 
 func main(){
   var name string
-  
+
 	fmt.Print("What is your name? > ")
 	fmt.Scanf("%s", &name)
 	fmt.Printf("Hello " + name +" this is GO World!!\n")
+  fmt.Printf(stringutil.Reverse(name) + "\n")
+
 }
